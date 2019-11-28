d3cd6f32272ac24cbd1d491828ab3b9c1d9b414f
Jeebe Diop <jeebe.diop@gmail.com>
2019-11-28 09:53:21 -0600

    formatted css, html, js
    formatted all the html, css, and js pages to 2 space indentation

--- javascript/maps.js
+++ javascript/maps.js
@@ -1,250 +1,250 @@
 function initMap() {
 
-    // Create a new StyledMapType object, passing it an array of styles,
-    // and the name to be displayed on the map type control.
-    var styledMapType = new google.maps.StyledMapType(
-        [{
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#1d2c4d"
-                }]
-            },
-            {
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#8ec3b9"
-                }]
-            },
-            {
-                "elementType": "labels.text.stroke",
-                "stylers": [{
-                    "color": "#1a3646"
-                }]
-            },
-            {
-                "featureType": "administrative.country",
-                "elementType": "geometry.stroke",
-                "stylers": [{
-                    "color": "#4b6878"
-                }]
-            },
-            {
-                "featureType": "administrative.land_parcel",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#64779e"
-                }]
-            },
-            {
-                "featureType": "administrative.province",
-                "elementType": "geometry.stroke",
-                "stylers": [{
-                    "color": "#4b6878"
-                }]
-            },
-            {
-                "featureType": "landscape.man_made",
-                "elementType": "geometry.stroke",
-                "stylers": [{
-                    "color": "#334e87"
-                }]
-            },
-            {
-                "featureType": "landscape.natural",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#023e58"
-                }]
-            },
-            {
-                "featureType": "poi",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#283d6a"
-                }]
-            },
-            {
-                "featureType": "poi",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#6f9ba5"
-                }]
-            },
-            {
-                "featureType": "poi",
-                "elementType": "labels.text.stroke",
-                "stylers": [{
-                    "color": "#1d2c4d"
-                }]
-            },
-            {
-                "featureType": "poi.park",
-                "elementType": "geometry.fill",
-                "stylers": [{
-                    "color": "#023e58"
-                }]
-            },
-            {
-                "featureType": "poi.park",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#3C7680"
-                }]
-            },
-            {
-                "featureType": "road",
-                "stylers": [{
-                    "visibility": "off"
-                }]
-            },
-            {
-                "featureType": "road",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#304a7d"
-                }]
-            },
-            {
-                "featureType": "road",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#98a5be"
-                }]
-            },
-            {
-                "featureType": "road",
-                "elementType": "labels.text.stroke",
-                "stylers": [{
-                    "color": "#1d2c4d"
-                }]
-            },
-            {
-                "featureType": "road.highway",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#2c6675"
-                }]
-            },
-            {
-                "featureType": "road.highway",
-                "elementType": "geometry.stroke",
-                "stylers": [{
-                    "color": "#255763"
-                }]
-            },
-            {
-                "featureType": "road.highway",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#b0d5ce"
-                }]
-            },
-            {
-                "featureType": "road.highway",
-                "elementType": "labels.text.stroke",
-                "stylers": [{
-                    "color": "#023e58"
-                }]
-            },
-            {
-                "featureType": "transit",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#98a5be"
-                }]
-            },
-            {
-                "featureType": "transit",
-                "elementType": "labels.text.stroke",
-                "stylers": [{
-                    "color": "#1d2c4d"
-                }]
-            },
-            {
-                "featureType": "transit.line",
-                "elementType": "geometry.fill",
-                "stylers": [{
-                    "color": "#283d6a"
-                }]
-            },
-            {
-                "featureType": "transit.station",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#3a4762"
-                }]
-            },
-            {
-                "featureType": "water",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#0e1626"
-                }]
-            },
-            {
-                "featureType": "water",
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#4e6d70"
-                }]
-            }
-        ], {
-            name: 'Styled Map'
-        });
-
-    // Create a map object, and include the MapTypeId to add
-    // to the map type control.
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 48.8566,
-            lng: 2.3522
-        },
-        zoom: 13.7,
-        tilt: 45,
-        rotateControl: true,
-        mapTypeControlOptions: {
-            mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
-                'styled_map'
-            ]
-        }
+  // Create a new StyledMapType object, passing it an array of styles,
+  // and the name to be displayed on the map type control.
+  var styledMapType = new google.maps.StyledMapType(
+    [{
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#1d2c4d"
+        }]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#8ec3b9"
+        }]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+          "color": "#1a3646"
+        }]
+      },
+      {
+        "featureType": "administrative.country",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+          "color": "#4b6878"
+        }]
+      },
+      {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#64779e"
+        }]
+      },
+      {
+        "featureType": "administrative.province",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+          "color": "#4b6878"
+        }]
+      },
+      {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+          "color": "#334e87"
+        }]
+      },
+      {
+        "featureType": "landscape.natural",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#023e58"
+        }]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#283d6a"
+        }]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#6f9ba5"
+        }]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+          "color": "#1d2c4d"
+        }]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [{
+          "color": "#023e58"
+        }]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#3C7680"
+        }]
+      },
+      {
+        "featureType": "road",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#304a7d"
+        }]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#98a5be"
+        }]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+          "color": "#1d2c4d"
+        }]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#2c6675"
+        }]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+          "color": "#255763"
+        }]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#b0d5ce"
+        }]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+          "color": "#023e58"
+        }]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#98a5be"
+        }]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+          "color": "#1d2c4d"
+        }]
+      },
+      {
+        "featureType": "transit.line",
+        "elementType": "geometry.fill",
+        "stylers": [{
+          "color": "#283d6a"
+        }]
+      },
+      {
+        "featureType": "transit.station",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#3a4762"
+        }]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#0e1626"
+        }]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#4e6d70"
+        }]
+      }
+    ], {
+      name: 'Styled Map'
     });
 
-    // Create a marker with school location
-    var favoriteLocation = {
+  // Create a map object, and include the MapTypeId to add
+  // to the map type control.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {
       lat: 48.8566,
       lng: 2.3522
-    };
-    var marker = new google.maps.Marker({
-        position: favoriteLocation,
-        map: map,
-        title: 'My Favorite Location - Paris'
-    });
+    },
+    zoom: 13.7,
+    tilt: 45,
+    rotateControl: true,
+    mapTypeControlOptions: {
+      mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+        'styled_map'
+      ]
+    }
+  });
 
-    // Create an info window
-    var contentString = '<div id="content">' +
-        '<div id="siteNotice">' +
-        '</div>' +
-        '<h1 id="firstHeading" class="firstHeading">My Favorite Place - Paris, France</h1>' +
-        '<div id="bodyContent">' +
-        '<p>It is my favorite places to visit' +
-        ' as I have a lot of family there and love the food.' +
-        ' It is also one of the few places where I can eat authentic French pastries' +
-        ' for breakfast.</p>'
+  // Create a marker with school location
+  var favoriteLocation = {
+    lat: 48.8566,
+    lng: 2.3522
+  };
+  var marker = new google.maps.Marker({
+    position: favoriteLocation,
+    map: map,
+    title: 'My Favorite Location - Paris'
+  });
+
+  // Create an info window
+  var contentString = '<div id="content">' +
+    '<div id="siteNotice">' +
     '</div>' +
-    '</div>';
+    '<h1 id="firstHeading" class="firstHeading">My Favorite Place - Paris, France</h1>' +
+    '<div id="bodyContent">' +
+    '<p>It is my favorite places to visit' +
+    ' as I have a lot of family there and love the food.' +
+    ' It is also one of the few places where I can eat authentic French pastries' +
+    ' for breakfast.</p>'
+  '</div>' +
+  '</div>';
 
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
 
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    });
+  marker.addListener('click', function () {
+    infowindow.open(map, marker);
+  });
 
 
-    //Associate the styled map with the MapTypeId and set it to display.
-    map.mapTypes.set('styled_map', styledMapType);
-    map.setMapTypeId('styled_map');
+  //Associate the styled map with the MapTypeId and set it to display.
+  map.mapTypes.set('styled_map', styledMapType);
+  map.setMapTypeId('styled_map');
 }
 
 var slideIndex = 1;
@@ -262,14 +262,18 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
